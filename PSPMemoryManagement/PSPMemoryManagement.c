f987f14e13388b7922ceea0c5c32aa3430e65e86
Jormii <lopeznatalj@gmail.com>
2019-09-10 17:27:32 +0200

    Implemented malloc, calloc, realloc & free

--- PSPMemoryManagement/PSPMemoryManagement.c
+++ PSPMemoryManagement/PSPMemoryManagement.c
@@ -2,7 +2,22 @@
 
 #include "./PSPMemoryManagement.h"
 
-void *PSPMalloc(size_t size)
+void *psp_malloc(size_t size)
 {
     return malloc(size);
+}
+
+void *psp_calloc(size_t n_items, size_t item_size)
+{
+    return calloc(n_items, item_size);
+}
+
+void *psp_realloc(void *pointer, size_t size)
+{
+    return realloc(pointer, size);
+}
+
+void psp_free(void *pointer)
+{
+    free(pointer);
 }
--- PSPMemoryManagement/PSPMemoryManagement.h
+++ PSPMemoryManagement/PSPMemoryManagement.h
@@ -1,1 +1,4 @@
-void *PSPMalloc(size_t size);
+void *psp_malloc(size_t size);
+void *psp_calloc(size_t n_items, size_t item_size);
+void *psp_realloc(void *pointer, size_t size);
+void psp_free(void *pointer);
