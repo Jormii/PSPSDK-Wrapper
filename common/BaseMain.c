8c6ee17b2e856e8ef424cfc61662cf82ff1f8ee9
Jormii <lopeznatalj@gmail.com>
2020-06-07 20:45:16 +0200

    Forgot to commit Main files

---
+++ common/BaseMain.c
@@ -0,0 +1,30 @@
+#include <pspkernel.h>
+#include <pspdisplay.h>
+#include "./callbacks.h"
+
+#define VERS 1
+#define REVS 0
+
+PSP_MODULE_INFO("PSPProgram", PSP_MODULE_USER, 1, 0);
+PSP_MAIN_THREAD_ATTR(PSP_THREAD_ATTR_USER);
+PSP_HEAP_SIZE_MAX();
+
+extern void psp_initialize();
+extern void psp_main_loop();
+extern void psp_deinitialize();
+
+int main()
+{
+    setupCallbacks();
+    psp_initialize();
+
+    while (running())
+    {
+        psp_main_loop();
+        sceDisplayWaitVblank();
+    }
+
+    psp_deinitialize();
+    sceKernelExitGame();
+    return 0;
+}
